d905b4e91a4bea69855baad19b362287dae8b23e
Андрій <gapeev1979@gmail.com>
2026-09-21 20:38:14 +0300

    add refresh

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -74,3 +74,45 @@ export const logoutUser = async (req, res) => {
 
   res.status(204).send();
 };
+
+export const refreshUserSession = async (req, res) => {
+  const { sessionId, refreshToken } = req.cookies;
+
+  if (!sessionId || !refreshToken) {
+    throw createHttpError(401, 'Missing session credentials');
+  }
+
+  // 1. Знаходимо поточну сесію за id сесії та рефреш токеном
+  const session = await Session.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+
+  // 2. Якщо такої сесії нема, повертаємо помилку
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  // 3. Якщо сесія існує, перевіряємо валідність рефреш токена
+  const isSessionTokenExpired = session.refreshTokenValidUntil < new Date();
+
+  // Якщо термін дії рефреш токена вийшов,
+  // видаляємо сесію і повертаємо помилку
+  if (isSessionTokenExpired) {
+    await session.deleteOne();
+    res.clearCookie('sessionId');
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+
+    throw createHttpError(401, 'Session token expired');
+  }
+
+  // 4. Якщо всі перевірки пройшли добре, видаляємо поточну сесію
+  await session.deleteOne();
+
+  // 5. Створюємо нову сесію та додаємо кукі
+  const newSession = await createSession(session.userId);
+  setSessionCookies(res, newSession);
+
+  res.status(200).json({ message: 'Session refreahed' });
+};
